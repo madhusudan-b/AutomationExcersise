a7e632b4bd78c6983c1e06f581d38909a9073e3a
madhusudan <madhusudansinghbisht38@gmail.com>
2025-08-25 21:40:22 +0530

    from eclipse

--- src/test/java/ReusableFunctions/PageAttribute.java
+++ src/test/java/ReusableFunctions/PageAttribute.java
@@ -2,7 +2,6 @@ package ReusableFunctions;
 
 import java.util.Properties;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
